e0915d4a43ad3a1291c9a6007041e1c5944a899a
Vahin Sharma <vahinsharma@gmail.com>
2022-03-05 12:28:14 +0530

    second commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,10 @@
 //! tegen is a Rust library designed to generate fast text from a template.
 //! # Example
 //! ```rs
-//! use tegen::TextGenerator;
+//! use tegen::tegen::TextGenerator;
 //! fn main() {
 //!     let tg = TextGenerator::new();
 //!     println!("{}", tg.generate("{Good {night|morning|evening|day}|Hello|Greetings|Howdy|What's up}, {friend|mate}! {How are you|How's it going}?".to_string()));
 //! }
 //! ```
 pub mod tegen;
-
-// fn example() {
-//     let tg = TextGenerator::new();
-
-//     println!("{}", tg.generate("{Good {night|morning|evening|day}|Hello|Greetings|Howdy|What's up}, {friend|mate}! {How are you|How's it going}?".to_string()));
-// }
